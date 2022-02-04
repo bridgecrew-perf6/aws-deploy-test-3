deeb0c2438cb6e3452a7738e0699a1033cdcd65b
code-scalper <tigerbaram@gmail.com>
2022-02-04 01:49:42 -0800

    controller

---
+++ src/main/java/com/deploytest/demo/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.deploytest.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+public class MainController {
+
+    @GetMapping("")
+    public String initialLoad(){
+        return "Hello AWS!";
+    }
+}
