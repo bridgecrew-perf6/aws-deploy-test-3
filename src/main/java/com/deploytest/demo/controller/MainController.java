93c54acca7b832728d32560e2802a1acfd38d0c2
code-scalper <tigerbaram@gmail.com>
2022-02-05 00:39:34 -0800

    last-deploy

--- src/main/java/com/deploytest/demo/controller/MainController.java
+++ src/main/java/com/deploytest/demo/controller/MainController.java
@@ -2,6 +2,7 @@ package com.deploytest.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/")
@@ -11,5 +12,10 @@ public class MainController {
     @GetMapping("")
     public String initialLoad(){
         return "Hello AWS!";
+    }
+
+    @GetMapping("/sayHello")
+    public String initialLoad(@RequestParam String word){
+        return "Hello "+word;
     }
 }
